8fb29340e1962bcac2daa8dde61afe97f1b2569e
extropyCoder <laurence@extropy.io>
2015-12-02 10:32:43 +0000

    Update and rename Register.sol to register.sol

--- ethereum/register.sol
+++ ethereum/register.sol
@@ -1,35 +1,12 @@
- contract User  {
+import "std.sol";
+import "user.sol";
 
-string name;
-string country;
-uint age;
-
-function User(string _name,uint _age){
-    name = _name;
-    age = _age;
-}
-
-function getAge() returns (uint){
-    return age;
-}
-
-
-}
-
-
-contract Teacher is User {
-
-    function Teacher(string _name){
-      
-    }
-
-}
 
 contract Solution{
    string description;
  
     
-   function Solution(string _description){
+   function createSolution(string _description){
        description = _description;
    }
    
@@ -85,42 +62,14 @@ contract Problem {
     
 }
 
-contract Team {
-    string teamName;
-    string teamMotto;
-    uint teamScore;
-    User[] members;
-    
-    enum Category{junior,middle,high}
-    uint minAge;
-    uint maxAge;
-    
-    
-    function Team(string _teamName,string _teamMotto,Teacher _teacher){
-        teamName = _teamName;
-        teamMotto = _teamMotto;
-        members.push(_teacher);
-    }
-    
-    function addMembers(User [] newMembers){
-            for (uint ii = 0;ii<newMembers.length;ii++){
-            if ((newMembers[ii].getAge() >= minAge) && (newMembers[ii].getAge() <= maxAge)){
-        members.push(newMembers[ii]);
-        
-            }
-            }
-    }
-    
-   
-}
-
 contract Register is named("Register")  {
     mapping(string=>address) users;
     mapping(address => uint) teams;
-    User teacher;
+    user teacher;
 
-function Register(string _teacherName) {
-    teacher = new User(_teacherName,100);
+function createRegister(string _teacherName) {
+    Teacher teacher = new Teacher();
+    teacher.createTeacher(_teacherName);
     
 }
 
@@ -133,10 +82,6 @@ function Register(string _teacherName) {
 contract Tests{
     
     function testRegister(){
-    
-    // create Register
-    Register register = new Register("teacher");
-    
         
         
     }
