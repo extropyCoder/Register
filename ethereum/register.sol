f729eaaa98fd908804b8c39f88d203be06a187dc
extropyCoder <laurence@extropy.io>
2015-12-03 07:33:06 +0000

    Update register.sol

--- ethereum/register.sol
+++ ethereum/register.sol
@@ -1,67 +1,6 @@
 import "std.sol";
 import "user.sol";
 
-
-contract Solution{
-   string description;
- 
-    
-   function createSolution(string _description){
-       description = _description;
-   }
-   
-    
-}
-
-
- 
-
-
-contract Problem {
-    string description;
-    uint solutionCount;
-
-    Solution []  juniorSolutions;
-    Solution []  middleSolutions;
-    Solution []  highSolutions;
-
-    mapping (address => uint) votes;    
-    
-    function voteJunior(uint _answerNumber){
-        address user = msg.sender;
-        votes[user] = _answerNumber;
-    }
-
-   function voteMiddle(uint _answerNumber){
-        address user = msg.sender;
-        votes[user] = _answerNumber;
-    }
-
-   function voteSenior(uint _answerNumber){
-        address user = msg.sender;
-        votes[user] = _answerNumber;
-    }
-    
-
-    // function createSolution(string _description){
-    //     Answer answer = new Answer(_description);
-    //     answerCount++;
-    //     answers[answerCount] = answer;
-    // }
-    
-    
-    // function getVotesForAnswer(uint _answerNumber) returns (uint){
-    //     for(uint ii=0;ii<answerCount;ii++){
-         
-    //     }
-        
-        
-        
-    // }
-    
-    
-}
-
 contract register is named("Register")  {
     mapping(string=>address) users;
     mapping(address => uint) teams;
