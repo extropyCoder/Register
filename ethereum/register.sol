8a7ee7b79ede4813c8915cdf6d30bec8bb457ed1
extropyCoder <laurence@extropy.io>
2015-12-03 08:25:12 +0000

    Update register.sol

--- ethereum/register.sol
+++ ethereum/register.sol
@@ -1,20 +1,27 @@
 import "std.sol";
 import "user.sol";
+import "problem.sol";
+
 
 contract register is named("Register")  {
     mapping(string=>address) users;
     mapping(address => uint) teams;
-    Problem[] problems;
+    problem[] problems;
     
 modifier userNotExists(string _userName,uint _age,string _country ) { if (users[_userName]==address(0x0)) _ }
 
 function createUser  (string _userName,uint _age,string _country ) userNotExists( _userName, _age, _country ) returns (address){
     user newUser = new user();
     newUser.createUserDetails(_userName,_age,_country);
-    
-  return newUser;
+    return newUser;
 }
 
+function createProblem(string _description){
+    Problem problem = new Problem();
+    problem.description = _description;
 }
+    
+}
+
 
 
