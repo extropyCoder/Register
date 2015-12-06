dbdf740db56bfcfdcfda4c90b7b8e3fffdca31f4
Laurence <laurence@planet97.com>
2015-12-06 12:36:14 +0000

    simplified and votes

--- ethereum/problem.sol
+++ ethereum/problem.sol
@@ -1,4 +1,88 @@
-import "team.sol";
+contract User {
+    string name;
+    string salt;
+    string hashPassword;
+    string country;
+    uint  age;
+    
+    
+    function User(string _name,string _salt,string _hashPassword,string _country,uint16 _age){
+        name = _name;
+        salt = _salt;
+        hashPassword = _hashPassword;
+        country = _country;
+        age = _age;
+    }
+    
+    function checkAge(uint16 _max,uint16 _min) returns (bool){
+        if ((age >=_min) && (age <=_max)){
+            return true;
+            
+        }
+        else{
+            return false;
+        }
+    }
+    
+    
+}
+
+contract Team {
+    string teamName;
+    string teamMotto;
+    uint teamScore;
+    User[] members;
+    
+    enum Category {junior,middle,senior}
+    Category category;
+    uint16 minAge;
+    uint16 maxAge;
+
+    
+    
+    function createTeam(string _teamName,string _teamMotto,User _teacher,Category _category){
+        teamName = _teamName;
+        teamMotto = _teamMotto;
+        members.push(_teacher);
+        category = _category;
+    }
+   
+    function createJuniorTeam(string _teamName,string _teamMotto,User _teacher){
+        createTeam( _teamName, _teamMotto, _teacher,Category.junior);
+        minAge = 7;
+        maxAge = 10; 
+    }
+    
+    function createMiddleTeam(string _teamName,string _teamMotto,User _teacher){
+        createTeam( _teamName, _teamMotto, _teacher,Category.middle);
+        minAge = 11;
+        maxAge = 14; 
+
+    }
+
+    function createSeniorTeam(string _teamName,string _teamMotto,User _teacher){
+        createTeam( _teamName, _teamMotto, _teacher,Category.senior);
+        minAge = 15;
+        maxAge = 18; 
+    }
+
+    
+    function addMembers(User [] newMembers){
+            for (uint ii = 0;ii<newMembers.length;ii++){
+                if(newMembers[ii].checkAge(minAge,maxAge)==true){
+                 members.push(newMembers[ii]);
+                }
+            }
+    }
+    
+   
+}
+
+
+
+
+
+
 contract Solution{
    string name;
    Team team;
@@ -15,6 +99,8 @@ contract Problem {
     uint deadline; 
    
     uint public juniorSolutionCount;
+    uint public middleSolutionCount;
+    uint public seniorSolutionCount;
     
    
     Solution []  juniorSolutions;
@@ -25,6 +111,9 @@ contract Problem {
     mapping (address => address) middleVotes;
     mapping (address => address) seniorVotes;
     
+    mapping(address => uint) public solutionVotes;
+    
+    
     modifier problemActive() {if (now<=deadline) _}
     
     function Problem(string _description,uint _deadline){
@@ -33,42 +122,56 @@ contract Problem {
     }
     
 
-    function voteJunior(Solution  _solution)  {
-        address user = msg.sender;
-        juniorVotes[user] = _solution;
+    function voteJunior(Solution  _solution,User _user)  {
+        address oldVote = juniorVotes[ _user];
+        solutionVotes[oldVote] --;
+        juniorVotes[_user] = _solution;
+        solutionVotes[_solution]++; 
     }
 
-    function voteMiddle(Solution  _solution) {
-        address user = msg.sender;
-        middleVotes[user] = _solution;
+    function voteMiddle(Solution  _solution,User _user) {
+        address oldVote = middleVotes[ _user];
+        solutionVotes[oldVote] --;
+        middleVotes[_user] = _solution;
+        solutionVotes[_solution]++; 
     }
 
-    function voteSenior(Solution  _solution)  {
-        address user = msg.sender;
-        seniorVotes[user] = _solution;
+    function voteSenior(Solution  _solution,User _user)  {
+        address oldVote = seniorVotes[ _user];
+        solutionVotes[oldVote] --;
+        seniorVotes[_user] = _solution;
+        solutionVotes[_solution]++; 
     }
 
 
-    function createJuniorSolution(string _description,Team _team)  {
+    function createJuniorSolution(string _description,Team _team) returns (address) {
         Solution newSolution =  new Solution(_description,_team);
         juniorSolutions.push(newSolution); 
         juniorSolutionCount++;
+        solutionVotes[newSolution] = 0;
+        return newSolution;
         
     }
-        function createMiddleSolution(string _description,Team _team)  {
+        function createMiddleSolution(string _description,Team _team) returns (address) {
         Solution newSolution =  new Solution(_description,_team);
-        juniorSolutions.push(newSolution);        
+        juniorSolutions.push(newSolution);     
+        middleSolutionCount++;
+        solutionVotes[newSolution] = 0;
+        return newSolution;
         
     }
-        function createSeniorSolution(string _description,Team _team)  {
+        function createSeniorSolution(string _description,Team _team) returns (address) {
         Solution newSolution =  new Solution(_description,_team);
-        juniorSolutions.push(newSolution);        
+        juniorSolutions.push(newSolution);
+        seniorSolutionCount++;
+        solutionVotes[newSolution] = 0;
+        return newSolution;
         
     }
 
-    function createSolution(string _description,Team _team) private returns (Solution){
-        Solution newSolution = new Solution(_description,_team);
-    }
+    // function createSolution(string _description,Team _team) private returns (Solution){
+    //     Solution newSolution = new Solution(_description,_team);
+    // }
     
  
  
@@ -76,7 +179,15 @@ contract Problem {
     return juniorSolutions[_number];
     
 }
-   
+ 
+    function getMiddleSolution(uint _number) returns (address){
+    return middleSolutions[_number];
+    
+}
+   function getSeniorSolution(uint _number) returns (address){
+    return seniorSolutions[_number];
+    
+}
    
     
 }
--- ethereum/register.sol
+++ ethereum/register.sol
@@ -1,28 +1,65 @@
 import "std.sol";
-import "user.sol";
 import "problem.sol";
 
 
 contract Register is named("Register") ,owned {
-    mapping(string=>address) users;
-    mapping(address => uint) teams;
+ //   mapping(string=>address) users;
+ //   mapping(address => uint) teams;
     Problem[] public problems;
+    User [] public users;
+    Team [] public teams;
     
-modifier userNotExists(string _userName ) { if (users[_userName]==address(0x0)) _ }
+    
+//modifier userNotExists(string _userName ) { if (users[_userName]==address(0x0)) _ }
 modifier userIsCreator() {if (msg.sender==owner) _}
 
 
-function createUser  (string _name,string _salt,string _hashPassword,string _country,uint16 _age ) userNotExists( _name) returns (address){
+// function createUser  (string _name,string _salt,string _hashPassword,string _country,uint16 _age ) userNotExists( _name) returns (address){
+//     User newUser = new User(  _name, _salt, _hashPassword, _country, _age);
+//     users.push(newUser);
+//     return newUser;
+// }
+
+
+
+function createUser  (string _name,string _salt,string _hashPassword,string _country,uint16 _age ) returns (address){
     User newUser = new User(  _name, _salt, _hashPassword, _country, _age);
+    users.push(newUser);
     return newUser;
 }
 
 
-function createProblem(string _description,uint _deadline) userIsCreator returns (address){
+function createJuniorTeam(string _teamName,string _teamMotto,User _teacher){
+    Team team = new Team();
+    team.createJuniorTeam( _teamName, _teamMotto, _teacher);
+    teams.push(team);
+}
+
+function createMiddleTeam(string _teamName,string _teamMotto,User _teacher){
+    Team team = new Team();
+    team.createMiddleTeam( _teamName, _teamMotto, _teacher);
+    teams.push(team);
+}
+function createSeniorTeam(string _teamName,string _teamMotto,User _teacher){
+    Team team = new Team();
+    team.createSeniorTeam( _teamName, _teamMotto, _teacher);
+    teams.push(team);
+}
+
+
+
+function createProblem(string _description,uint _deadline)  returns (address){
     Problem newProblem = new Problem(_description,_deadline);
+    problems.push(newProblem);
     
 }
+  
+//   function createSolution(string _name,Team _team)  returns (address){
+//     Solution newSolution = new Solution(_name,_team);
+   
     
+// }
+      
     
     
     
